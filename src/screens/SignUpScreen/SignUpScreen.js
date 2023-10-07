3ee5e8fe1f66b1e48ee972cf487b387093e09fd2
Puja Reddy <pujareddyt2004@gmail.com>
2023-10-07 03:28:47 -0400

    added styling

--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -43,4 +43,59 @@ const Signup = ({naviation}) => {
   );
 };
 
+const styles = {
+    container: {
+        flex: 1,
+        backgroundColor: '#d496a7',
+        paddingHorizontal: 20,
+        paddingVertical: 50,
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    hero: {
+        backgroundColor: 'white',
+        borderRadius: 16,
+        padding: 16,
+    },
+    space: {
+        height: 40,
+    },
+    heroImage: {
+        width: '100%',
+        height: 400,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 8,
+        color: 'black',
+    },
+    input: {
+        marginBottom: 15,
+        backgroundColor: '#f2d3e1',
+        paddingHorizontal: 12,
+        paddingVertical: 10,
+        borderRadius: 8,
+        color: 'black',
+    },
+    button: {
+        backgroundColor: '#d496a7',
+        paddingVertical: 12,
+        paddingHorizontal: 14,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 12,
+        marginTop: 15,
+    },
+    buttonText: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#fff',
+    },
+};
+
 export default Signup;
