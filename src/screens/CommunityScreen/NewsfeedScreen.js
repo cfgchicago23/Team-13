405976ac0fd2d9aec3f22f5831f45738d188577c
Hadi Khamsi <hadik8360@gmail.com>
2023-10-07 02:41:37 -0500

    Added notis
    Added notis

--- src/screens/CommunityScreen/NewsfeedScreen.js
+++ src/screens/CommunityScreen/NewsfeedScreen.js
@@ -1,9 +1,103 @@
-import { Text } from "react-native-paper"
+import * as React from "react";
+import { View, ScrollView } from "react-native";
+import {
+  Avatar,
+  Button,
+  Card,
+  Text,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "#d496a7", // Background Pink
+    primary: "#cc8b86", // Title Daughter Project Pink
+    text: "#5d576b", // Text Dark Purple
+    white: "#f1f7f9", // Text White
+  },
+};
+
+const LeftContent = ({ avatarImage }) => (
+  <Avatar.Image size={40} source={avatarImage} />
+);
 
 export default function NewsFeed() {
+  const newsfeedData = [
+    {
+      userName: "Admin 1",
+      postTitle: "Tips to Prevent Child Trafficking",
+      postContent:
+        "Here are some important tips to prevent child trafficking in your community.",
+      postImage: "https://picsum.photos/700",
+      avatarImage: require("./avatar.jpg"),
+      likeCount: 15,
+    },
+    {
+      userName: "Admin 2",
+      postTitle: "Community Building Event",
+      postContent:
+        "Join us for a community-building event this Saturday. Let's work together to protect children.",
+      postImage: "https://picsum.photos/701",
+      avatarImage: require("./avatar2.jpeg"),
+      likeCount: 10,
+    },
+    {
+      userName: "Admin 3",
+      postTitle: "Providing Family-Based Care",
+      postContent:
+        "Learn how we provide family-based care to children who are victims of abuse and exploitation.",
+      postImage: "https://picsum.photos/702",
+      avatarImage: require("./avatar3.jpeg"),
+      likeCount: 20,
+    },
+    {
+      userName: "Admin 4",
+      postTitle: "Report Suspicious Activity",
+      postContent:
+        "If you see any suspicious activity related to child trafficking, please report it immediately.",
+      postImage: "https://picsum.photos/703",
+      avatarImage: require("./avatar4.avif"),
+      likeCount: 5,
+    },
+  ];
+
   return (
-    <>
-      <Text>NewsFeed</Text>
-    </>
-  )
+    <PaperProvider theme={theme}>
+      <ScrollView>
+        {newsfeedData.map((item, index) => (
+          <Card key={index} style={{ margin: 16 }}>
+            <Card.Title
+              title={item.userName}
+              subtitle="2 hours ago"
+              left={() => <LeftContent avatarImage={item.avatarImage} />}
+            />
+            <Card.Content>
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                {item.postTitle}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: theme.colors.text,
+                  marginBottom: 10,
+                }}
+              >
+                {item.postContent}
+              </Text>
+            </Card.Content>
+            <Card.Cover source={{ uri: item.postImage }} />
+            <Card.Actions>
+              <Button icon="heart" color={theme.colors.primary}>
+                {item.likeCount}
+              </Button>
+              <Button>Comment</Button>
+            </Card.Actions>
+          </Card>
+        ))}
+      </ScrollView>
+    </PaperProvider>
+  );
 }
--- src/screens/CommunityScreen/NotificationsScreen.js
+++ src/screens/CommunityScreen/NotificationsScreen.js
@@ -1,9 +1,74 @@
-import { Text } from "react-native-paper";
+import React from 'react';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import { Avatar, Card, Text } from 'react-native-paper';
+
+const notificationsData = [
+  {
+    logo: require('./alert.png'),
+    count: 3,
+    message: 'You have 3 new alerts',
+  },
+  {
+    logo: require('./bulb.jpg'),
+    count: 1,
+    message: 'A new idea for you!',
+  },
+  {
+    logo: require('./check.png'),
+    count: 0,
+    message: 'All tasks completed.',
+  },
+];
 
 export default function Notifications() {
-  return(
-    <>
-      <Text>Notifications</Text>
-    </>
-  )
+  return (
+    <ScrollView>
+      {notificationsData.map((item, index) => (
+        <Card key={index} style={styles.card}>
+          <Card.Content style={styles.cardContent}>
+            <Avatar.Image size={40} source={item.logo} />
+            <View style={styles.notificationContent}>
+              <Text style={styles.notificationMessage}>{item.message}</Text>
+              {item.count > 0 && (
+                <View style={styles.notificationBadge}>
+                  <Text style={styles.badgeText}>{item.count}</Text>
+                </View>
+              )}
+            </View>
+          </Card.Content>
+        </Card>
+      ))}
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 16,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  notificationContent: {
+    marginLeft: 16,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  notificationMessage: {
+    flex: 1,
+  },
+  notificationBadge: {
+    backgroundColor: '#d496a7',
+    borderRadius: 12,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+  },
+  badgeText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
