69922fe16e6179cb040ecc7a4f8718a62d6c1a20
Hadi Khamsi <hadik8360@gmail.com>
2023-10-06 23:59:03 -0500

    Finished the UI for the gethelp
    Finished the UI for the gethelp

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
-import HelpScreen from "./src/screens/HelpScreen/HelpScreen";
+import GetHelp from "./src/screens/HelpScreen/GetHelp";
 // import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { PaperProvider } from 'react-native-paper';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { PaperProvider } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 import Landing from "./src/screens/LandingScreen/LandingScreen";
-import Education from './src/screens/EducationScreen/EducationScreen';
-import { initFirebaseApp } from './src/utils/firebase';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AuthPage } from './src/screens/LoginScreen/LoginScreen';
-const app = initFirebaseApp()
-const Stack = createNativeStackNavigator()
+import Education from "./src/screens/EducationScreen/EducationScreen";
+import { initFirebaseApp } from "./src/utils/firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthPage } from "./src/screens/LoginScreen/LoginScreen";
+const app = initFirebaseApp();
+const Stack = createNativeStackNavigator();
 
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Landing" component={Landing} />
+//         <Stack.Screen name="Education" component={Education} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Landing" component={ Landing } />
-        <Stack.Screen name="Education" component={ Education } />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+// export function AppWrapped() {
+//   return (
+//     <PaperProvider>
+//       <App />
+//     </PaperProvider>
+//   );
+// }
 
-export function AppWrapped() {
+export default function HelpScreen() {
   return (
     <PaperProvider>
-      <App />
+      <GetHelp />
     </PaperProvider>
   );
 }
