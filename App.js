3e641a66acdc51019a4fdd411d4cbbf2f923b32a
AadityaBhoota <aadityabhoota@gmail.com>
2023-10-06 20:24:35 -0500

    updating login screen

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { PaperProvider, Text } from 'react-native-paper';
+
+import React, { useState } from 'react';
+import { PaperProvider, Text, TextInput} from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
 import Landing from "./src/screens/LandingScreen/LandingScreen";
 import { initFirebaseApp } from './src/utils/firebase';
 import { AuthPage } from './src/screens/LoginScreen/LoginScreen';
 
-function App() {
+export default function App() {
+  
   return (
-    <AuthPage />
+   <AuthPage/>
   );
 }
 
-export default function AppWrapped() {
+export function AppWrapped() {
   return (
     <PaperProvider>
       <App />
--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,28 +1,38 @@
 
-import { PaperProvider, Text, TextInput} from 'react-native-paper';
+import React, { useState, useCallback } from 'react';
+import { PaperProvider, Text, TextInput, Button} from 'react-native-paper';
 
 export const AuthPage = () => {
-    const [userName, setUserName] = useState()
-    const [password, setPassword] = useState()
+    const[userName,setUserName] = useState("");
+    const[passWord,setPassword] = useState("");
+
+const sendData=useCallback(()=>{
+    
+},[])
+
 
     return (
-    <>
+        <>
         <Text>
             this is a test
         </Text>
-
+    
         <TextInput
           label="Username: "
-          value={"Username"}
-          onChangeText={text => setUserName(userName)}
+          value={userName}
+          onChange={(event)=>{setUserName(event.target.value)}}
         />
-
+    
         <TextInput
           label="Password: "
-          value={"Password"}
-          onChangeText={text => setPassword(password)}
+          value={passWord}
+          onChange={(event)=>{setPassword(event.target.value)}}
         />
 
+        <Button mode="contained" onPress={sendData}>
+            Log In
+        </Button>
       </>
     );
 };
+
