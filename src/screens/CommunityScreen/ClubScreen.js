f11f94f5da36aef4b5ad585a563a188da056c569
AadityaBhoota <aadityabhoota@gmail.com>
2023-10-07 04:05:55 -0500

    initial club screen code

--- src/screens/CommunityScreen/ClubScreen.js
+++ src/screens/CommunityScreen/ClubScreen.js
@@ -1,9 +1,29 @@
-export default function Club(props) {
-  const club = props.club
+import { Appbar as PaperAppbar, Text, Searchbar, Card, Avatar, IconButton, Caption } from "react-native-paper"
+import { useEffect, useState, React } from "react";
+import { StyleSheet } from "react-native";
+import { collection, getDocs } from "firebase/firestore"
+import { getFirebaseStore } from "../../utils/firebase";
+import { useRoute } from '@react-navigation/native';  
 
-  return(
+
+export default function Club() {
+  const route = useRoute();
+  const { club } = route.params;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [clubs, setClubs] = useState([]);
+
+  return (
     <>
-      
+      {/* Club name */}
+      <PaperAppbar.Header>
+        <PaperAppbar.BackAction onPress={() => {}} />
+        <PaperAppbar.Content 
+            title = {name}
+        />
+      </PaperAppbar.Header>
+      {/* Club location */}
+      {/* number of members */}
+
     </>
-  )
+  );
 }
