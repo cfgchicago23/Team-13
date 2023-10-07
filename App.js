aef58cbc2fa84488af4033978bd3a08becf1a423
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-10-06 19:26:56 -0500

    fixed landing

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
 import Landing from "./src/screens/LandingScreen/LandingScreen";
 import { initFirebaseApp } from './src/utils/firebase';
 
-initFirebaseApp()
+const app = initFirebaseApp()
 
-export default function App() {
+function App() {
   return (
-    <PaperProvider>
-        
-    </PaperProvider>
+    <Landing />
   );
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function AppWrapped() {
+  return (
+    <PaperProvider>
+        <App />
+    </PaperProvider>
+  );
+}
 
